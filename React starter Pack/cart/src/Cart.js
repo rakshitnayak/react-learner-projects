7905ea94bfe3115a34358d5374051327524dda49
rakshit <rdsonnn@gmail.com>
2021-04-12 12:30:32 +0530

    add handleIncreaseQuantity

--- React starter Pack/cart/src/Cart.js
+++ React starter Pack/cart/src/Cart.js
@@ -37,12 +37,31 @@ class cart extends React.Component {
       ],
     };
   }
+
+  handleIncreaseQuantity = (product) => {
+    console.log("hey please increase qty", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products,
+    });
+  };
+
   render() {
     const { products } = this.state;
     return (
       <div className="cart">
         {products.map((product) => {
-          return <CartItem product={product} key={product.id} />;
+          return (
+            <CartItem
+              product={product}
+              key={product.id}
+              onIncreaseQuantity={this.handleIncreaseQuantity}
+            />
+          );
         })}
       </div>
     );
--- React starter Pack/cart/src/CartItem.js
+++ React starter Pack/cart/src/CartItem.js
@@ -1,57 +1,6 @@
 import React from "react";
 
 class CartItem extends React.Component {
-  //  this.increaseQuantity = this.increaseQuantity.bind(this); we should use this while we use normal functions. not necssary while using arrow functions
-  // this.testing();
-
-  // testing() {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve("done");
-  //     }, 5000);
-  //   });
-
-  //   promise.then(() => {
-  //     this.setState({ qty: this.state.qty + 10 });
-  //     this.setState({ qty: this.state.qty + 10 });
-  //     this.setState({ qty: this.state.qty + 10 });
-  //     console.log("state", this.state);
-  //   });
-  // }
-
-  increaseQuantity = () => {
-    // console.log("this", this.state);
-
-    //set state  form 1
-    // this.setState({
-    //   qty: this.state.qty + 1,
-    // },()=>{});//second argument to remove asynchronous.
-
-    //set state form 2
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
-
-  decreaseQuantity = () => {
-    const { qty } = this.state;
-    if (qty === 0) {
-      return;
-    }
-    this.setState(
-      (prevState) => {
-        return {
-          qty: prevState.qty - 1,
-        };
-      },
-      () => {
-        console.log("this", this.state); //to remove asynchronous this is given by react as a second argument.
-      }
-    );
-  };
-
   render() {
     const { price, title, qty } = this.props.product;
     return (
@@ -67,13 +16,13 @@ class CartItem extends React.Component {
             {/* buttons */}
 
             <img
-              src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1617283132~hmac=47d39eeb199eeffddb4df93bbb6d1f0e"
+              src="https://www.flaticon.com/svg/vstatic/svg/1237/1237946.svg?token=exp=1618208682~hmac=363d70cc285adb5f5951fd1e618eb6dd"
               className="action-icons"
               alt="increase"
-              onClick={this.increaseQuantity}
+              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
             />
             <img
-              src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1617283100~hmac=cc688418d68b44040f2be60cfa2480a2"
+              src="https://www.flaticon.com/premium-icon/icons/svg/2801/2801932.svg"
               className="action-icons"
               alt="decrease"
               onClick={this.decreaseQuantity}
